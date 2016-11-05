1c0d907f625165a1667d08d9057e9328f1e9b254
Danny <danny.gosain@hotmail.com>
2016-11-05 18:39:04 -0400

    Added sample page with white bg

---
+++ assets/js/incrButton.js
@@ -0,0 +1,8 @@
+$('#minus').click(function() {
+    $('#output').html(function(i, val) { return val-1 });
+});
+
+$('#plus').click(function() {
+    $('#output').html(function(i, val) { return val+1 });
+});
+							
